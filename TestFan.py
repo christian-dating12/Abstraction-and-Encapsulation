81c7a7275db9a358ebe7e58db552bab7f8ba14ce
chrismdating1206@gmail.com <christian.dating12>
2023-07-04 22:08:52 +0800

    second object

--- TestFan.py
+++ TestFan.py
@@ -7,4 +7,5 @@ class TestFan:
 # For the first object, assign the maximum speed, radius 10, color yellow, and turn it on.
         first_obj = (Fan.FAST, 10, "yellow", True)
 # Assign medium speed, radius 5, color blue, and turn it off for the second object. 
+        second_obj = (Fan.MEDIUM, 5, "blue", False)
 # Display each object’s speed, radius, color, and on properties.

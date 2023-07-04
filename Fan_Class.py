0504253e1c2f5baad5bc2d2321781eae1f494e22
chrismdating1206@gmail.com <christian.dating12>
2023-07-04 20:50:36 +0800

    pseudocode

--- Fan_Class.py
+++ Fan_Class.py
@@ -1,2 +1,11 @@
 class Fan:
     pass
+
+# Pseudocode
+# Three constants named SLOW, MEDIUM, and FAST with the values 1, 2, and 3 to denote the fan speed.
+# A constructor that creates a fan with the specified speed (default SLOW), radius (default 5), color (default blue), and on (default False).
+# A private int data field named speed that specifies the speed of the fan.
+# A private bool data field named on that specifies whether the fan is on (the default is False).
+# A private float data field named radius that specifies the radius of the fan.
+# A private string data field named color that specifies the color of the fan.
+# The accessor(getters) and mutator(setters) methods for all four data fields.

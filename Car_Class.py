b9e18c1fbdbd2b9146d1c40e7df025356ab1913b
chrismdating1206@gmail.com <christian.dating12>
2023-07-04 22:50:15 +0800

    The accelerate method should add 5 to the speed data attribute each time it is called

--- Car_Class.py
+++ Car_Class.py
@@ -4,8 +4,10 @@ class Car:
         self.__year_model = year_model
         self.__make = make
         self.__speed = speed
-        
+
 # Pseudocode
 # The accelerate method should add 5 to the speed data attribute each time it is called.
+    def acc(self):
+        self.__speed += 5
 # The brake method should subtract 5 from the speed data attribute each time it is called.
 # The get_speed method should return the current speed.

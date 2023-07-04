dbb36c365786888467a2f3f8d3718133d43f2987
chrismdating1206@gmail.com <christian.dating12>
2023-07-04 22:07:27 +0800

    first object

--- TestFan.py
+++ TestFan.py
@@ -1,9 +1,10 @@
 from Fan_Class import Fan
 
 class TestFan:
-    pass
+    def __init__(self):
 
 # Pseudocode
-# For the first object, assign the maximum speed, radius 10, color yellow, and turn it on. 
+# For the first object, assign the maximum speed, radius 10, color yellow, and turn it on.
+        first_obj = (Fan.FAST, 10, "yellow", True)
 # Assign medium speed, radius 5, color blue, and turn it off for the second object. 
 # Display each object’s speed, radius, color, and on properties.

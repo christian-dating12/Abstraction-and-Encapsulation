f5c602d6d971fd6370cc182d0eaf7ac6ea316b63
chrismdating1206@gmail.com <christian.dating12>
2023-07-04 21:58:31 +0800

    pseudocode

--- TestFan.py
+++ TestFan.py
@@ -2,3 +2,8 @@ from Fan_Class import Fan
 
 class TestFan:
     pass
+
+# Pseudocode
+# For the first object, assign the maximum speed, radius 10, color yellow, and turn it on. 
+# Assign medium speed, radius 5, color blue, and turn it off for the second object. 
+# Display each object’s speed, radius, color, and on properties.

e24f2a6be222782896a5f7f425cf8dd33efb979b
chrismdating1206@gmail.com <christian.dating12>
2023-07-05 22:01:32 +0800

    get the current speed

--- Car.py
+++ Car.py
@@ -1,7 +1,12 @@
 from Car_Class import Car
 
+car_obj = Car (2023, "Ford")
 # Pseudocode
+
 # Call the accelerate method five times. 
 # After each call to the accelerate method, get the current speed of the car and display it.
+for i in range(1, 6):
+    car_obj.acc()
+    print("CURRENT SPEED: ", car_obj.get_speed())
 # Then call the brake method five times. 
 # After each call to the brake method, get the current speed of the car and display it.

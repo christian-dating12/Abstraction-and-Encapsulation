e65f826d59a867674c4d43714e0c09289960f3b3
chrismdating1206@gmail.com <christian.dating12>
2023-07-05 23:13:28 +0800

    colors and designs for Pet Class

--- Pet.py
+++ Pet.py
@@ -1,10 +1,21 @@
 from Pet_Class import Pet
 
 # Pseudocode
+
+input("\n\033[93mPress Any Key to Start...\033[0m\n")
+print("\033[90m=" *100)
+print("\033[93m ------------------------- WELCOME! ------------------------- \033[0m".center(105))
+print("\033[90m=" *100)
+
 # Prompt the user to enter the name, type, and age of his or her pet. 
 # This data should be stored as the object’s attributes. 
 
-pet = Pet (input("Enter the name of your pet: "), input("Enter the type of your pet: "), input("Enter the age of your pet: "))
+pet = Pet (input("\n\033[92mEnter the name of your pet: "), input("\n\033[92mEnter the type of your pet: "), input("\n\033[92mEnter the age of your pet: "))
+
+print("\033[90m=" *100)
 
 # Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.
-print("\nPET'S NAME: ", pet.get_name(), "\nPET'S TYPE: ", pet.get_animal_type(), "\nPET'S AGE: ", pet.get_age())
+print("\n\n\033[95mPET'S NAME: ", pet.get_name(), "\n\n\033[95mPET'S TYPE: ", pet.get_animal_type(), "\n\n\033[95mPET'S AGE: ", pet.get_age())
+
+print("\033[90m=" *100)
+print("\033[90m=" *100)

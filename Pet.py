0307a6aab8ee0152bfcd4ed74af2567efe6f2b34
chrismdating1206@gmail.com <christian.dating12>
2023-07-05 22:36:21 +0800

    pseudocode

---
+++ Pet.py
@@ -0,0 +1,4 @@
+# Pseudocode
+# Prompt the user to enter the name, type, and age of his or her pet. 
+# This data should be stored as the object’s attributes. 
+# Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.

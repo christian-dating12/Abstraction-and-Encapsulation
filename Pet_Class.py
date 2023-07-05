65a1dffd31dc27d6b811778ca947b6debd83eaf1
chrismdating1206@gmail.com <christian.dating12>
2023-07-05 22:22:18 +0800

    set name

--- Pet_Class.py
+++ Pet_Class.py
@@ -1,5 +1,4 @@
 class Pet:
-    pass
 
 # Pseudocode
 
@@ -9,8 +8,12 @@ class Pet:
         self.__age = age
 # It should also have the following methods:
 # set_name() This method assigns a value to the _ _name field.
-# set_animal_type() This method assigns a value to the _ _animal_type field.
-# set_age() This method assigns a value to the _ _age field.
+    def set_name(self, name):
+        self.__new = name
+
 # get_name() This method returns the value of the _ _ name field.
+
+# set_animal_type() This method assigns a value to the _ _animal_type field.
 # get_animal_type() This method returns the value of the _ _animal_type field.
+# set_age() This method assigns a value to the _ _age field.
 # get_age() This method returns the value of the _ _age field.

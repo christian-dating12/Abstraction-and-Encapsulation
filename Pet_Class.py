18475466fc79c60a91ba169ef90f62230dd3e14a
chrismdating1206@gmail.com <christian.dating12>
2023-07-05 22:11:32 +0800

    pseudocode

---
+++ Pet_Class.py
@@ -0,0 +1,11 @@
+class Pet:
+    pass
+
+# Pseudocode
+# It should also have the following methods:
+# set_name() This method assigns a value to the _ _name field.
+# set_animal_type() This method assigns a value to the _ _animal_type field.
+# set_age() This method assigns a value to the _ _age field.
+# get_name() This method returns the value of the _ _ name field.
+# get_animal_type() This method returns the value of the _ _animal_type field.
+# get_age() This method returns the value of the _ _age field.

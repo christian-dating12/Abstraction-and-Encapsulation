610e848c9e571ed67ceeb2f9bfb5b817d89b936e
chrismdating1206@gmail.com <christian.dating12>
2023-07-05 22:48:00 +0800

    Prompt the user to enter the name, type, and age of his or her pet

--- Pet.py
+++ Pet.py
@@ -1,4 +1,10 @@
+from Pet_Class import Pet
+
 # Pseudocode
 # Prompt the user to enter the name, type, and age of his or her pet. 
 # This data should be stored as the object’s attributes. 
+
+pet = Pet (input("Enter the name of your pet: "), input("Enter the type of your pet: "), input("Enter the age of your pet: "))
+
 # Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.
+

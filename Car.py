fd743349c5e4d136b3ed9439661f6f610fd7e3f8
chrismdating1206@gmail.com <christian.dating12>
2023-07-05 22:04:35 +0800

    get the current speed (brake)

--- Car.py
+++ Car.py
@@ -10,3 +10,6 @@ for i in range(1, 6):
     print("CURRENT SPEED: ", car_obj.get_speed())
 # Then call the brake method five times. 
 # After each call to the brake method, get the current speed of the car and display it.
+for i in range(1, 6):
+    car_obj.brake()
+    print("CURRENT SPEED: ", car_obj.get_speed())

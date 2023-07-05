6a5b9fc557247dea859253df137b78ce297679e5
chrismdating1206@gmail.com <christian.dating12>
2023-07-05 23:21:31 +0800

    designs and colors for Car Class

--- Car.py
+++ Car.py
@@ -3,13 +3,26 @@ from Car_Class import Car
 car_obj = Car (2023, "Ford")
 # Pseudocode
 
+input("\n\033[93mPress Any Key to Start...\033[0m\n")
+print("\033[90m=" *100)
+print("\033[93m ------------------------- WELCOME! ------------------------- \033[0m".center(105))
+print("\033[90m=" *100)
+
 # Call the accelerate method five times. 
 # After each call to the accelerate method, get the current speed of the car and display it.
+
+print("\033[93m ACCELERATE \033[0m".center(105))
+print("\033[90m=" *100)
+
 for i in range(1, 6):
     car_obj.acc()
-    print("CURRENT SPEED: ", car_obj.get_speed())
+    print("\033[97mCURRENT SPEED: ", car_obj.get_speed())
 # Then call the brake method five times. 
 # After each call to the brake method, get the current speed of the car and display it.
+print("\033[90m=" *100)
+print("\033[93m BRAKE  \033[0m".center(105))
+print("\033[90m=" *100)
 for i in range(1, 6):
     car_obj.brake()
-    print("CURRENT SPEED: ", car_obj.get_speed())
+
+    print("\033[97mCURRENT SPEED: ", car_obj.get_speed())

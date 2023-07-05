c00e59ae39ac9eac8b1886ea8f2917618945774d
chrismdating1206@gmail.com <christian.dating12>
2023-07-05 21:49:37 +0800

    pseudocode

--- Car.py
+++ Car.py
@@ -1,1 +1,7 @@
 from Car_Class import Car
+
+# Pseudocode
+# Call the accelerate method five times. 
+# After each call to the accelerate method, get the current speed of the car and display it.
+# Then call the brake method five times. 
+# After each call to the brake method, get the current speed of the car and display it.
